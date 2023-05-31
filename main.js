94d1667b36f944dd6cde891513d5c41eba6c6054
Damien Monasterios <86851423+PixelDroid19@users.noreply.github.com>
2023-05-30 21:19:54 -0500

    Cambios de variables

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const Button = document.querySelector(".key_button");
+const Prueba = "";
 
 Button.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     const key = e.target;
     const value = key.dataset.valor;
-    //console.log()
   }
 });
